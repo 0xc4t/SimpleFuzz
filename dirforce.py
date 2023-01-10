35958a114fc9f089979b25b2fa0291b68de9b4a8
Varel valensio <105418279+varelsecurity@users.noreply.github.com>
2023-01-10 07:56:28 +0700

    Update dirforce.py

--- dirforce.py
+++ dirforce.py
@@ -1,3 +1,5 @@
+# Import modul
+
 import os
 import sys
 import argparse
@@ -5,6 +7,7 @@ import requests as r
 from colorama import Fore
 
 os.system('clear')
+
 parser = argparse.ArgumentParser()
 
 print(Fore.LIGHTCYAN_EX ,'''
@@ -15,6 +18,7 @@ print(Fore.LIGHTCYAN_EX ,'''
 ''')
 parser.add_argument('-w','--wordlist', type=str, required=True,help="Masukan Wordlist")
 parser.add_argument('-u','--url', type=str, required=True, help="Masukan URL")
+parser.add_argument('-o','--output', type=str, required=True, help="Masukan OUTPUT (eg. name.txt)")
 args = parser.parse_args()
 
 os.system("clear")
@@ -28,7 +32,8 @@ print (Fore.LIGHTCYAN_EX, '''
 print("Author : Varel Security")
 print("")
 print("[+] Wordlist: ",args.wordlist)
-print("[+] URL: ", args.url)
+print("[+] Url: ", args.url)
+print("[+] Output: ", args.output)
 print("")
 
 list = open(args.wordlist)
@@ -40,9 +45,18 @@ for i in word:
     url_path = (f"{args.url}{line}")
     if request.status_code == 200:
         print (Fore.GREEN,f"(Status: {request.status_code}) [Size : {len(request.content)}] \t{url_path}")
+        f = open(f"200.{args.output}","a")
+        f.write(f"(Status: {request.status_code}) [Size : {len(request.content)}] \t{url_path}\n")
     elif request.status_code ==404:
         print (Fore.RED,f"(Status: {request.status_code}) [Size : {len(request.content)}] \t{url_path}")
+        a = open(f"404.{args.output}","a")
+        a.write(f"(Status: {request.status_code}) [Size : {len(request.content)}] \t{url_path}\n")
     elif request.status_code==301:
         print (Fore.BLUE,f"(Status: {request.status_code}) [Size : {len(request.content)}] \t{url_path}")
+        b = open(f"301.{args.output}","a")
+        b.write(f"(Status: {request.status_code}) [Size : {len(request.content)}] \t{url_path}\n")
     elif request.status_code == 403:
         print (Fore.LIGHTYELLOW_EX ,f"(Status: {request.status_code}) [Size : {len(request.content)}] \t{url_path}")
+        c = open(f"403.{args.output}","a")
+        c.write(f"(Status: {request.status_code}) [Size : {len(request.content)}] \t{url_path}\n")
+
