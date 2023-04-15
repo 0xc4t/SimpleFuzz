af791095e731506a2208d59c45825f4294967b03
varel <105418279+VarelSecurity@users.noreply.github.com>
2023-04-15 23:01:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,22 @@ Y='\033[1;33m'
 N='\033[0m'
 
 printf "${Y}Installasi....${N}"
+
 sudo apt-get install python3 python3-pip 
 pip3 install -r requirements.txt
 
 printf  "${Y}Config...${N}"
+
 chmod +x sfuzz
 chmod +x msfuzz
 sudo mv sfuzz msfuzz /usr/bin
 
 printf  "${Y}Path...${N}"
+
 cd ..
 sudo mv SimpleFuzz /usr/share/
 
 printf "${Y}Installasi berhasil...${N}"
+
 printf "${Y}jalankan tools dengan perintah, sfuzz -h dan msfuzz -h ${N}"
+
