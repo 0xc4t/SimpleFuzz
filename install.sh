ec442074bde35aacd8d60b0485a594fdb7ab87b3
varel <105418279+0xkucing@users.noreply.github.com>
2023-11-10 22:49:24 +0700

    Update

--- install.sh
+++ install.sh
@@ -1,18 +1,7 @@
-Y='\033[1;33m'
-N='\033[0m'
-
-echo"${Y}Installasi....${N}"
-
 sudo apt-get install python3 python3-pip 
 pip3 install -r requirements.txt
-
-
-echo "${Y}Path...${N}"
-
+sudo chmod +x sfuzz
 sudo cp sfuzz /usr/bin
 sudo cp sfuzz /usr/local/bin
 
-echo"${Y}Installasi berhasil...${N}"
-
-echo"${Y}jalankan tools dengan perintah, sfuzz -h ${N}"
-
+echo "SUCCESS"
